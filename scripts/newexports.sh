de37f589685c27c2eda6e12f6cc7f198aa07bb18
Nick Stephens <stenic05@evergreen.edu>
2014-12-15 15:47:42 -0800

    Add environment specific export script as an example

---
+++ scripts/newexports.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+export PYTHONPATH=/data/local/tmp/org.hello.world/files/lib/python2.7.zip:/data/local/tmp/org.hello.world/files/lib/python2.7:/data/local/tmp/org.hello.world/files/lib/python2.7/lib-dynload/:/data/local/tmp/org.hello.world/files/lib/python2.7/site-packages/
+export ANDROID_PRIVATE=/data/local/tmp/org.hello.world/files
+export PYTHONHOME=/data/local/tmp/org.hello.world
+export TEMP=/storage/sdcard0/Android/data/com.sensibilitytestbed/files/extras/tmp
+export PYTHONOPTIMIZE=2
+export LD_LIBRARY_PATH=/data/local/tmp/org.hello.world/lib:/data/local/tmp/lib
